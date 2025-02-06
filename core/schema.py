23ad2756d175a7843cbbcd31f0dc3e39c54be91b
Bernapai <juanemeunier@gmail.com>
2025-02-05 22:26:52 -0300

    Changes 1.5 Add mutation

--- core/schema.py
+++ core/schema.py
@@ -2,22 +2,62 @@ import graphene
 from graphene_django.types import DjangoObjectType
 from restaurant.models import Restaurant, Table, Order    
 
-class RestaurantType(graphene.ObjectType):
+class RestaurantType(graphene.DjangoObjectType):
     class meta:
         model = Restaurant
         fields = ("name", "description")
 
-class TableType(graphene.ObjectType):
+class TableType(graphene.DjangoObjectType):
     class meta:
         model = Table
         fields = "__all__"
 
-class OrderType(graphene.ObjectType):
+class OrderType(graphene.DjangoObjectType):
     class meta:
         model = Order
         fields = "__all__"
 
+
+
+#Metodos de mutacion POST
+class CreateRestaurantMutation(graphene.Mutation):
+    class Arguments:
+        name = graphene.String()
+        description = graphene.String()
+    Restaurant = graphene.Field(RestaurantType)
     
+    def mutate(self, info, name, description):
+        restaurant = Restaurant(name=name, description=description)
+        restaurant.save()
+        return CreateRestaurantMutation(restaurant=restaurant)
+
+class CreateTableMutation(graphene.Mutation):
+    class Arguments:
+        number = graphene.Int()
+        restaurant = graphene.String()
+        quantity = graphene.Int()
+    Table = graphene.Field(TableType)
+    
+    def mutate(self, info, number, restaurant, quantity):
+        table = Table(number=number, restaurant=restaurant, quantity=quantity)
+        table.save()
+        return CreateTableMutation(table=table)
+
+class CreateOrderMutation(graphene.Mutation):
+    class Arguments:
+        table = graphene.Int()
+        restaurant = graphene.String()
+        order_time = graphene.String()
+    Order = graphene.Field(OrderType)
+    
+    def mutate(self, info, table, restaurant, order_time):
+        order = Order(table=table, restaurant=restaurant, order_time=order_time)
+        order.save()
+        return CreateOrderMutation(order=order)
+    
+
+
+
 class Query(graphene.ObjectType):
     hello = graphene.String(name=graphene.String(default_value="stranger"))
     all_restaurants = graphene.List(RestaurantType)
@@ -49,6 +89,12 @@ class Query(graphene.ObjectType):
             raise ValueError(f"Error inesperado: {str(e)}")
 
 
-    
+
+
+
+class Mutation(graphene.ObjectType):
+    CreateRestaurant = CreateRestaurantMutation.Field()
+    CreateTable = CreateTableMutation.Field()
+    CreateOrder = CreateOrderMutation.Field()
 
 schema = graphene.Schema(query=Query)
